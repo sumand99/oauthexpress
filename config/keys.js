129a5292b2147cf308c8dd69dfdad77de6e9357a
sumand99 <sumand99@192.168.1.6>
2020-06-13 05:44:28 -0700

    Added all the changes to support Oauth

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+//add this files to .gitignore
+
+module.exports = {
+  google: {
+    clientID:'29108398133-rh2s388k8sk2j2t71cobupouglu8i13l.apps.googleusercontent.com',
+    clientSecret:'qkJk0W2JkoKMg7D_O5KI9dP8'
+  },
+  mongodb: {
+    dbURI: 'mongodb+srv://dba:test@cluster0-ik3en.mongodb.net/teachindia?retryWrites=true&w=majority'
+  },
+session: {
+  cookieKey: 'thequickbrownfoxjumpsrightoverthelazydog'
+}
+};
