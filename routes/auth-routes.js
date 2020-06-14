a56f75acc780c4b127e229f4bdbc548e7c666a80
sumand99 <sumand99@192.168.1.6>
2020-06-14 01:58:25 -0700

    fixed the redirect issue.

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -22,6 +22,7 @@ router.get('/google', passport.authenticate('google', {
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
      res.send(req.user);
     //res.redirect('/profile');
+    //res.send('You reached the callback URI');
 });
 
 module.exports = router;
